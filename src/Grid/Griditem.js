57037a6e28cb1a293541c45d93158d8706427286
Matt Nev <mattnev@microsoft.com>
2021-11-19 13:50:58 -0600

    add files after completing lesson

---
+++ src/Grid/Griditem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Griditem.module.css';
+import Badge from './Badge';
+import Explorer from './Explorer';
+
+const Griditem = (props) => {
+  return (
+ <div className={styles.card} onClick={() => props.updateClicked(props.fact.id)}>
+    {props.fact.clicked ? <Explorer /> : <Badge />}
+    <h2 className={styles.title}>{props.fact.title}</h2>
+    <article className={styles.fact}>{props.fact.fact}</article>
+ </div>
+  )
+}
+
+export default Griditem;
+  
